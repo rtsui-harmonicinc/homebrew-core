71cd3e32176769954f6d935961f5a366b5a374f2
Sean Molenaar <smillerdev@me.com>
2026-08-17 14:39:21 +0200

    mise: add network sandboxing

--- Formula/m/mise.rb
+++ Formula/m/mise.rb
@@ -33,6 +33,9 @@ class Mise < Formula
     depends_on "openssl@3"
   end
 
+  # downloads crates
+  deny_network_access! [:postinstall, :test]
+
   def install
     # Ensure that the `openssl` crate picks up the intended library.
     ENV["OPENSSL_DIR"] = formula_opt_prefix("openssl@3")
